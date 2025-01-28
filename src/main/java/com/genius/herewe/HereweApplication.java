c273091f4d3b666a101cc25f2c5dd25701dd73ce
SSung023 <hat7206@naver.com>
2025-01-28 15:04:33 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/genius/herewe/HereweApplication.java
@@ -0,0 +1,13 @@
+package com.genius.herewe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genius/herewe/HereweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.herewe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
