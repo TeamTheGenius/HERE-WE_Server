b1b5197e917e183a1a809871b4b8043da580e5f0
SSung023 <hat7206@naver.com>
2025-01-29 21:37:00 +0900

    feat: OAuth2에 필요한 파일 생성
    - OAuth2에 필요한 Config 클래스/Handler/Service 생성

---
+++ src/main/java/com/genius/herewe/security/oauth/handler/OAuth2FailureHandler.java
@@ -0,0 +1,20 @@
+package com.genius.herewe.security.oauth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        
+    }
+}
