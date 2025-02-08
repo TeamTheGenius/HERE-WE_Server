9171f188e97e72dff24603c892275035e99613e1
HEY <50323157+SSung023@users.noreply.github.com>
2025-02-08 22:41:51 +0900

    [FEAT] 파일 시스템 개발 (#12)
    * feat: Files 엔티티 작성 - #11
    
    - 파일의 정보를 담을 Files 엔티티 작성
    - 파일 시스템의 환경 정보를 담고 있는 FileEnv enum class 작성
    - 해당 파일이 어떤 정보를 담는 FileType enum class 작성
    
    * chore: AWS S3 의존성 추가 - #11
    
    * feat: 로컬 파일 저장소 구현 - #11
    
    - 파일 업로드, 삭제, 갱신 기능 구현
    - 파일 조회(Base64 encoded 문자열로 반환) 기능 구현
    - 파일 관련 유틸 클래스 구현
    
    * feat: Files 레포지토리 인터페이스 생성 - #11
    
    * feat: AWS S3 파일 저장소 구현 - #11
    
    - S3Config 클래스를 통해 S3에 필요한 값 설정
    - FilesStorage에서 불필요한 메서드(validateFileExist()) 삭제
    - BusinessException 생성자 추가 구현
    
    * feat: FilesManager 클래스 구현
    
    - 비지니스 로직을 담고 있는 Manager 클래스 구현
    - FilesResponse 응답 클래스 구현
    
    * feat: 파일 컨트롤러 작성 - #11
    
    - 파일 등록/수정/삭제 API 구현
    - 파일을 저장해야 하는 클래스에 대하여 FileHolder 인터페이스 구현
    
    * fix: 파일 업데이트 버그 수정
    
    - 저장한 사진에 대하여 사진 변경이 되지 않는 버그 수정
    - FilesStorage에 parameter로 전달하는 FileDTO를 잘못 생성하고 있었음
    
    * refactor: FilesStorage에 불필요한 코드 제거
    
    * chore: yml 값 갱신 및 프로퍼티 위치 변경
    
    * chore: yml 값 갱신 및 프로퍼티 위치 변경
    
    * chore: Update git submodule
    
    * chore: 필요없는 코드 삭제

---
+++ src/main/java/com/genius/herewe/file/domain/FileEnv.java
@@ -0,0 +1,6 @@
+package com.genius.herewe.file.domain;
+
+public enum FileEnv {
+	LOCAL,
+	CLOUD
+}
---
+++ src/main/java/com/genius/herewe/file/domain/FileHolder.java
@@ -0,0 +1,7 @@
+package com.genius.herewe.file.domain;
+
+public interface FileHolder {
+	Files getFiles();
+
+	void setFiles(Files files);
+}
