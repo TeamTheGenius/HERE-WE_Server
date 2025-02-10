f8dbf3dfacacc00c00ed791e291a7a6ad7d7f45c
SSung023 <hat7206@naver.com>
2025-02-11 07:25:04 +0900

    refactor: 회원가입 API 응답 구조 변경
    - 사용자 PK, 닉네임, 프로필 사진 정보 전달하도록 변경

--- src/main/java/com/genius/herewe/user/controller/UserController.java
+++ src/main/java/com/genius/herewe/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.genius.herewe.user.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import com.genius.herewe.user.dto.SignupRequest;
 import com.genius.herewe.user.dto.SignupResponse;
 import com.genius.herewe.user.facade.UserFacade;
 import com.genius.herewe.util.response.CommonResponse;
+import com.genius.herewe.util.response.SingleResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,8 +29,8 @@ public class UserController {
 	}
 
 	@PostMapping("/auth/signup")
-	public CommonResponse signup(@RequestBody SignupRequest signupRequest) {
-		SignupResponse signup = userFacade.signup(signupRequest);
-		return CommonResponse.ok();
+	public SingleResponse<SignupResponse> signup(@RequestBody SignupRequest signupRequest) {
+		SignupResponse signupResponse = userFacade.signup(signupRequest);
+		return new SingleResponse<>(HttpStatus.CREATED, signupResponse);
 	}
 }
