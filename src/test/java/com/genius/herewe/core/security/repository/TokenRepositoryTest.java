81018c38b54c358e15fad65bfc51a1ad8f3d1166
SSung023 <hat7206@naver.com>
2025-02-24 07:06:11 +0900

    test: TokenRepository 테스트 코드 작성
    - Embedded에서 TestContainers로 방식 변경
    - DCI 패턴을 적용하여 테스트 코드 작성
    - 테스트 코드에서 사용할 Fixture 클래스 작성

---
+++ src/test/java/com/genius/herewe/core/security/repository/TokenRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.genius.herewe.core.security.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.genius.herewe.core.security.domain.Token;
+import com.genius.herewe.core.security.fixture.TokenFixture;
+
+@SpringBootTest
+@Testcontainers
+public class TokenRepositoryTest {
+	@Container
+	static GenericContainer<?> redis = new GenericContainer<>("redis:7.0")
+		.withExposedPorts(6379);
+	@Autowired
+	private TokenRepository tokenRepository;
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", redis::getHost);
+		registry.add("spring.redis.port", redis::getFirstMappedPort);
+	}
+
+	@Nested
+	@DisplayName("저장되어 있는 Token 엔티티에 대해")
+	class Describe_saved_token {
+		Token token = TokenFixture.createDefault();
+
+		@BeforeEach
+		void init() {
+			tokenRepository.save(token);
+		}
+
+		@Nested
+		@DisplayName("userId를 통해 Token 조회를 시도할 때")
+		class Context_try_find_token {
+			@Test
+			@DisplayName("token이 존재하지 않는다면 Optional.empty()를 반환한다.")
+			public void it_return_empty_entity() {
+				Optional<Token> foundToken = tokenRepository.findById(2L);
+				assertThat(foundToken).isEmpty();
+			}
+
+			@Test
+			@DisplayName("token이 존재한다면 Optional로 감싸서 반환한다.")
+			public void it_return_present_entity() {
+				Optional<Token> foundToken = tokenRepository.findById(token.getUserId());
+
+				//then
+				assertThat(foundToken).isPresent();
+				assertThat(foundToken.get().getUserId()).isEqualTo(token.getUserId());
+				assertThat(foundToken.get().getNickname()).isEqualTo(token.getNickname());
+				assertThat(foundToken.get().getToken()).isEqualTo(token.getToken());
+			}
+		}
+
+		@Nested
+		@DisplayName("delete() 메서드를 통해 삭제를 시도할 때")
+		class Context_try_delete_token {
+			@Test
+			@DisplayName("userId에 해당하는 Key가 있다면 삭제가 완료된다.")
+			public void it_delete_successfully() {
+				tokenRepository.delete(token.getUserId());
+
+				Optional<Token> foundToken = tokenRepository.findById(token.getUserId());
+				assertThat(foundToken).isEmpty();
+			}
+		}
+	}
+}
