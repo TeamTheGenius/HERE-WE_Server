4b6782c8ed10ee012d2136cf00ff9ab5ccf056bb
SSung023 <hat7206@naver.com>
2025-03-10 10:36:26 +0900

    docs: DTO에 대해 swagger 어노테이션 적용

--- src/main/java/com/genius/herewe/business/crew/dto/CrewCreateRequest.java
+++ src/main/java/com/genius/herewe/business/crew/dto/CrewCreateRequest.java
@@ -1,15 +1,19 @@
 package com.genius.herewe.business.crew.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "크루 생성 시 요청 객체")
 public record CrewCreateRequest(
 	@NotNull(message = "이름은 필수 항목입니다")
 	@NotBlank(message = "이름은 공백일 수 없습니다")
+	@Schema(description = "크루 이름", example = "눈물나게 맛있는거 먹고싶은 사람들")
 	String name,
 
 	@Size(max = 1000)
+	@Schema(description = "크루 소개글", example = "수도권의 맛집들을 뽀개는 동아리입니다!")
 	String introduce
 ) {
 }
--- src/main/java/com/genius/herewe/business/crew/dto/CrewPreviewResponse.java
+++ src/main/java/com/genius/herewe/business/crew/dto/CrewPreviewResponse.java
@@ -2,9 +2,15 @@ package com.genius.herewe.business.crew.dto;
 
 import com.genius.herewe.business.crew.domain.Crew;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "크루의 간략한 정보를 담은 응답 객체")
 public record CrewPreviewResponse(
+	@Schema(description = "크루의 식별자(PK)", example = "1")
 	Long crewId,
+	@Schema(description = "크루의 이름", example = "눈물나게 맛있는거 먹고싶은 사람들")
 	String name,
+	@Schema(description = "현재까지의 크루 참여 인원", example = "13")
 	int participantCount
 ) {
 
