6e811b90e9513a56e0d82b5007ffd7b881d26a51
SSung023 <hat7206@naver.com>
2025-03-15 19:33:33 +0900

    refactor: 메일 주소 yml 값으로 변경

--- src/main/java/com/genius/herewe/infra/mail/service/MailManager.java
+++ src/main/java/com/genius/herewe/infra/mail/service/MailManager.java
@@ -1,5 +1,6 @@
 package com.genius.herewe.infra.mail.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
@@ -10,13 +11,18 @@ import org.thymeleaf.context.Context;
 import com.genius.herewe.core.global.exception.BusinessException;
 import com.genius.herewe.infra.mail.dto.MailRequest;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class MailManager {
+	private final String MAIL_ADDRESS;
 	private final JavaMailSender mailSender;
 	private final TemplateEngine templateEngine;
+
+	public MailManager(@Value("${spring.mail.username}") String MAIL_ADDRESS,
+		JavaMailSender mailSender, TemplateEngine templateEngine) {
+		this.MAIL_ADDRESS = MAIL_ADDRESS;
+		this.mailSender = mailSender;
+		this.templateEngine = templateEngine;
+	}
 
 	public void send(MailRequest mailRequest) {
 		Context context = new Context();
@@ -34,7 +40,7 @@ public class MailManager {
 				String content = templateEngine.process("crew-invitation", context);
 
 				helper.setTo(mailRequest.receiverMail());
-				helper.setFrom("genius.herewe@gmail.com", "HERE:WE");
+				helper.setFrom(MAIL_ADDRESS, "HERE:WE");
 				helper.setSubject("[HERE:WE] 크루 초대가 도착했습니다 :)");
 
 				helper.setText(content, true);
