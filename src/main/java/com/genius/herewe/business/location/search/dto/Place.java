aa31695116b27b9efca225581ee93e8b420bd7a9
SSung023 <hat7206@naver.com>
2025-03-19 22:08:34 +0900

    feat: 응답 객체에 kakao map url 포함

--- src/main/java/com/genius/herewe/business/location/search/dto/Place.java
+++ src/main/java/com/genius/herewe/business/location/search/dto/Place.java
@@ -11,18 +11,21 @@ public record Place(
 	@Schema(description = "이름")
 	@JsonProperty(value = "place_name")
 	String name,
-	@Schema(description = "지번 주소")
-	@JsonProperty(value = "address_name")
-	String address,
-	@Schema(description = "도로명 주소")
-	@JsonProperty(value = "road_address_name")
-	String roadAddress,
 	@Schema(description = "해당 장소의 x 좌표")
 	@JsonProperty(value = "x")
 	Double x,
 	@Schema(description = "해당 장소의 y 좌표")
 	@JsonProperty(value = "y")
 	Double y,
+	@Schema(description = "지번 주소")
+	@JsonProperty(value = "address_name")
+	String address,
+	@Schema(description = "도로명 주소")
+	@JsonProperty(value = "road_address_name")
+	String roadAddress,
+	@Schema(description = "kakao map url")
+	@JsonProperty(value = "place_url")
+	String placeUrl,
 	@Schema(description = "전화번호")
 	@JsonProperty(value = "phone")
 	String phone
